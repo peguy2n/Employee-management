3330bc65db97da7a72852b09c92f8b8d5f14c460
peguy <66145536+peguy2n@users.noreply.github.com>
2020-08-04 01:12:09 -0700

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,3 +1,21 @@
+#Table creation code
+
+CREATE TABLE Departments (
+  Code INTEGER PRIMARY KEY,
+  Name varchar(255) NOT NULL ,
+  Budget decimal NOT NULL 
+);
+
+CREATE TABLE Employees (
+  SSN INTEGER PRIMARY KEY,
+  Name varchar(255) NOT NULL ,
+  LastName varchar(255) NOT NULL ,
+  Department INTEGER NOT NULL , 
+  foreign key (department) references Departments(Code) 
+) ENGINE=INNODB;
+ 
+ # SQL Exercises/
+
 #Select the last name of all employees.
 SELECT LastName FROM Employees;
 
